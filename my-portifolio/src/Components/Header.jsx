b82e4ff83bfb5c8244ce972d8b741d05c6de7ccb
valmir <valmirchagas88@gmail.com>
2022-10-12 01:24:33 -0300

    Update link nav bar

--- my-portifolio/src/Components/Header.jsx
+++ my-portifolio/src/Components/Header.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from 'react';
+// import { Link } from 'react-router-dom';
 
 const Header = () => {
   const links = ["Home", "About", "Projects", "Skills", "Contacts"];
   return (
     <div className='header-container'>
-      <div className='name'><h2>Valmir Chagas</h2></div>
-      <nav>
+      <div className='name'><h2><span>V</span>ALMIR<span> C</span>HAGAS</h2></div>
+      <nav className='nav-container'>
         <ul className='links-container'>
-          {links.map((link) => <li>{link}</li>)}
+          {links.map((link) => (
+            <li key={link}>
+              {link}
+            </li>
+          ))}
         </ul>
       </nav>
     </div>
