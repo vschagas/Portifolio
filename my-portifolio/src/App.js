0689bff47594d06514cb6bb5182e0f26ded51676
valmir <valmirchagas88@gmail.com>
2022-10-13 09:08:15 -0300

    Inicial config body

--- my-portifolio/src/App.js
+++ my-portifolio/src/App.js
@@ -1,11 +1,11 @@
-// import './Style/App.css';
-
+import Body from "./Components/Body";
 import Header from "./Components/Header";
 
 function App() {
   return (
     <div>
       <Header />
+      <Body />
     </div>
   );
 }
--- my-portifolio/src/index.js
+++ my-portifolio/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './Style/App.css';
+import './Style/App.css'
+import './Style/header.css';
+import './Style/body.css'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
