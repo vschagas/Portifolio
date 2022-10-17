cdc2f9ef9aa5b26cf56cedfa88c8f573759ae87e
valmir <valmirchagas88@gmail.com>
2022-10-17 01:39:18 -0300

    Update body

--- my-portifolio/src/Components/Body.jsx
+++ my-portifolio/src/Components/Body.jsx
@@ -46,10 +46,17 @@ const Body = () => {
   return (
     <div className='body-container'>
       <div className='text-container'>
-        <p>Coding is <span class="typed-text"></span><span class="cursor">&nbsp;</span></p>
+        {/* <p>Coding is <span class="typed-text"></span><span class="cursor">&nbsp;</span></p> */}
+        <div>
+          <p>Olá, meu nome é</p>
+          <h1>Valmir Chagas</h1>
+          <h1>Desenvolvedor</h1>
+        </div>
+        <p>sou desenvolvedor back-end (e ocasionalmente front-end)</p>
+        <button className='download-button'>Download CV</button>
       </div>
       <div className='image-container'>
-        <img className='perfil-image' src="./"/>
+        {/* <img className='perfil-image' src="./"/> */}
       </div>
     </div>
   )
