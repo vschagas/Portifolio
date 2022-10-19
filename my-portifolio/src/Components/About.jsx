7da9bbc47475545c734d887ce030e627996138a1
valmir <valmirchagas88@gmail.com>
2022-10-19 01:31:48 -0300

    update about me

--- my-portifolio/src/Components/About.jsx
+++ my-portifolio/src/Components/About.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='about-container'>About</div>
+    <div className='about-container'>
+      About
+    </div>
   )
 }
 
---
+++ my-portifolio/src/data/About.js
@@ -0,0 +1,7 @@
+export const aboutData = {
+  title: 'Sobre mim',
+  description1: 'Meu nome é Valmir Chagas sou desenvolvedor front-end, for',
+  description2: 'A Trybe é uma escola de tecnologia, que proporciona uma formação com mais de 1500 horas, abordando fundamentos de desenvolvimento Web, Front-end, Back-end, ciência da computação, metodologias ágeis e habilidades comportamentais.',
+  description3: 'O mundo da Tecnologia está em constante transformação, e é por isso que na Trybe estudamos Ferramentas e Tecnologias como:  JavaScript, HTML 5, Css, Git, React, Redux, Jest, RTL, Docker, MySql, NodeJs, TypeScript, MongoDb e Python.',
+  image: 1,
+}; export default aboutData;
