7a76d2f57fb47709611f889f7983fc016655c747
valmir <valmirchagas88@gmail.com>
2022-10-20 00:08:48 -0300

    Update about

--- my-portifolio/src/Components/About.jsx
+++ my-portifolio/src/Components/About.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { aboutData } from '../data/About'
 
 const About = () => {
+  const { name, description1 } = aboutData;
   return (
     <div className='about-container'>
-      About
+      <div className='image-about'>image</div>
+      <div className='article-about'>
+        <article>
+          <h3>{name}</h3>
+          <br />
+          <p>{description1}</p>
+        </article>
+        
+      </div>
     </div>
   )
 }
--- my-portifolio/src/data/About.js
+++ my-portifolio/src/data/About.js
@@ -1,6 +1,7 @@
 export const aboutData = {
+  name: 'Valmir Chagas',
   title: 'Sobre mim',
-  description1: 'Meu nome é Valmir Chagas sou desenvolvedor front-end, for',
+  description1: 'Minha carreira em Ti inicia em meados de 2010, quando me formei em Técnico em Automação Indústrial pelo IFSP. trabalhei com programação de CLP, manutenção eletrônica e cheguei até me aventurar em robótica. Após alguns anos migrei para o desenvolvimento de pessoas onde trabalhei na IBM e atuei como coordenador técnico. no início de 2022 decidi aprofundar meus conhecimentos em desenvolvimento web. E pra isso escolhi a TRYBE pra me acompanhar nesse processo devido a estrutura do curso que é desenvolvida com base no que o mercado exige dos profissionais em hardSkills e sofSkills.',
   description2: 'A Trybe é uma escola de tecnologia, que proporciona uma formação com mais de 1500 horas, abordando fundamentos de desenvolvimento Web, Front-end, Back-end, ciência da computação, metodologias ágeis e habilidades comportamentais.',
   description3: 'O mundo da Tecnologia está em constante transformação, e é por isso que na Trybe estudamos Ferramentas e Tecnologias como:  JavaScript, HTML 5, Css, Git, React, Redux, Jest, RTL, Docker, MySql, NodeJs, TypeScript, MongoDb e Python.',
   image: 1,
