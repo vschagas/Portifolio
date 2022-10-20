83e7a295e1fd11bb6939d4e284592da6b2c91ca1
valmir <valmirchagas88@gmail.com>
2022-10-20 01:13:45 -0300

    Inserted Image

--- my-portifolio/src/Components/About.jsx
+++ my-portifolio/src/Components/About.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import { aboutData } from '../data/About'
+import React from 'react';
+import { aboutData } from '../data/About';
+
 
 const About = () => {
-  const { name, description1 } = aboutData;
+  const { name, description1, description2, description3, image } = aboutData;
   return (
     <div className='about-container'>
-      <div className='image-about'>image</div>
+      <div className='image-about'>
+        <img src={image} alt='valmir' />
+      </div>
       <div className='article-about'>
         <article>
           <h3>{name}</h3>
           <br />
           <p>{description1}</p>
+          <br />
+          <p>{description2}</p>
+          <br />
+          <p>{description3}</p>
         </article>
         
       </div>
--- my-portifolio/src/data/About.js
+++ my-portifolio/src/data/About.js
@@ -1,8 +1,10 @@
+import logo from '../images/perfilImage.jpg'
+ 
 export const aboutData = {
   name: 'Valmir Chagas',
   title: 'Sobre mim',
-  description1: 'Minha carreira em Ti inicia em meados de 2010, quando me formei em Técnico em Automação Indústrial pelo IFSP. trabalhei com programação de CLP, manutenção eletrônica e cheguei até me aventurar em robótica. Após alguns anos migrei para o desenvolvimento de pessoas onde trabalhei na IBM e atuei como coordenador técnico. no início de 2022 decidi aprofundar meus conhecimentos em desenvolvimento web. E pra isso escolhi a TRYBE pra me acompanhar nesse processo devido a estrutura do curso que é desenvolvida com base no que o mercado exige dos profissionais em hardSkills e sofSkills.',
-  description2: 'A Trybe é uma escola de tecnologia, que proporciona uma formação com mais de 1500 horas, abordando fundamentos de desenvolvimento Web, Front-end, Back-end, ciência da computação, metodologias ágeis e habilidades comportamentais.',
-  description3: 'O mundo da Tecnologia está em constante transformação, e é por isso que na Trybe estudamos Ferramentas e Tecnologias como:  JavaScript, HTML 5, Css, Git, React, Redux, Jest, RTL, Docker, MySql, NodeJs, TypeScript, MongoDb e Python.',
-  image: 1,
+  description1: 'Minha carreira em Ti inicia em meados de 2010, quando me formei em Técnico em Automação Indústrial pelo IFSP. trabalhei com programação de CLP, manutenção eletrônica e cheguei até me aventurar em robótica. Após alguns anos migrei para o desenvolvimento de pessoas onde trabalhei na IBM e atuei como coordenador técnico. No início de 2022 decidi aprofundar meus conhecimentos em desenvolvimento web.',
+  description2: 'Pra isso escolhi a TRYBE pra me acompanhar nesse processo,devido a estrutura do curso que é desenvolvida com base no que o mercado exige dos profissionais em hardSkills e sofSkills. São mais de 1500 horas, abordando fundamentos de desenvolvimento Web, Front-end, Back-end, ciência da computação, metodologias ágeis e habilidades comportamentais.',
+  description3: 'O mundo da Tecnologia está em constante transformação, e é por isso que na TRYBE estudamos Ferramentas e Tecnologias como:  JavaScript, HTML 5, Css, Git, React, Redux, Jest, RTL, Docker, MySql, NodeJs, TypeScript, MongoDb e Python.',
+  image: logo
 }; export default aboutData;
